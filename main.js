9a9ff9f13aacad1883cc6e43d421a88caeef5e1e
AnakinLalwani <72091898+AnakinLalwani@users.noreply.github.com>
2021-04-13 13:00:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function tm() {
+    window.location.href = "https://anakinlalwani.github.io/Class103/";
+}
+function mn() {
+    window.alert('Oops! The mobilenet website has not yet been finished!')
+}
